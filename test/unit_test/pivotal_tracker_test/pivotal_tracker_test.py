fc549469ce9f7853de0c10609b0e1b13ad0aba88
Teguh Prabowo <pair+teguh@vidio.com>
2022-06-01 08:51:41 +0000

    change mock request using 3rd party lib
    * Refactor common tests using parameterized

--- test/unit_test/pivotal_tracker_test/pivotal_tracker_test.py
+++ test/unit_test/pivotal_tracker_test/pivotal_tracker_test.py
@@ -1,60 +1,53 @@
-import unittest
-from unittest import mock
+import re
 
 import pytest
 from exception import StoryNotFound
 from pivotal_tracker import PivotalTracker
 from pivotal_tracker.story import Story
-from requests import Response
 
 
-# This method will be used by the mock to replace requests.get
-def mocked_requests_get(status: int, json: dict = {}, text: str = ""):
-    response = mock.Mock(Response)
-    response.status_code = status
-    response.json = mock.Mock(return_value=json)
-    response.text = text
-
-    return response
+@pytest.fixture
+def pt() -> PivotalTracker:
+    return PivotalTracker(str(123), str(123))
 
 
-class PivotalTrackerGetStoryTestCase(unittest.TestCase):
-    @mock.patch(
-        "requests.get",
-        return_value=mocked_requests_get(
-            200, {"id": 1, "name": "Story name", "story_type": "feature"}
+@pytest.mark.parametrize(
+    "story_id,expected_id,expected_title,expected_type",
+    ([1, 1, "Story name", "feature"], [1, 1, "Story name 3", "bug"]),
+)
+def test_get_story(
+    requests_mock,
+    pt: PivotalTracker,
+    story_id: int,
+    expected_id: int,
+    expected_title: str,
+    expected_type: str,
+):
+    requests_mock.get(
+        re.compile(
+            rf"https:\/\/www.pivotaltracker.com/(.*)/stories/{story_id}"
         ),
+        json={
+            "id": expected_id,
+            "name": expected_title,
+            "story_type": expected_type,
+        },
     )
-    def test_get_story(self, mock_get):
-        pt = PivotalTracker(123, 123)
-        story = pt.get_story(11)
 
-        assert isinstance(story, Story)
-        assert story.id == 1
-        assert story.title == "Story name"
-        assert story.type == "feature"
+    story = pt.get_story(story_id)
 
-    @mock.patch(
-        "requests.get",
-        return_value=mocked_requests_get(
-            200, {"id": 2, "name": "Story name 3", "story_type": "bug"}
-        ),
+    assert isinstance(story, Story)
+    assert story.id == expected_id
+    assert story.title == expected_title
+    assert story.type == expected_type
+
+
+def test_not_found_story(requests_mock, pt: PivotalTracker):
+    requests_mock.get(
+        re.compile(r"https:\/\/www.pivotaltracker.com/(.*)/stories/11"),
+        status_code=404,
+        text="Story not found",
     )
-    def test_get_story2(self, mock_get):
-        pt = PivotalTracker(123, 123)
-        story = pt.get_story(11)
 
-        assert isinstance(story, Story)
-        assert story.id == 2
-        assert story.title == "Story name 3"
-        assert story.type == "bug"
-
-    @mock.patch(
-        "requests.get",
-        return_value=mocked_requests_get(404, text="Story not found"),
-    )
-    def test_not_found_story(self, mock_get):
-        pt = PivotalTracker(123, 123)
-
-        with pytest.raises(StoryNotFound):
-            pt.get_story(11)
+    with pytest.raises(StoryNotFound):
+        pt.get_story(11)
